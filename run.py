5718ddb2605d7dab0fa748fc812c6bee92938e6d
Walter Wu <walterwuyan@gmail.com>
2023-04-10 10:03:30 +0100

    Update run cmd

--- run.py
+++ run.py
@@ -25,6 +25,14 @@ c = f"C:\\nonsys\\workspace\\GfxExp\\build\\bin\\Debug\\neural_radiance_caching.
     -inst scene \
     -name rectlight0 -emittance 1200 0 0 -rectangle 0.1 0.1 \
     -begin-pos -0.2 1.05 2.586 -end-pos -0.2 1.05 3.0 -freq 5 -time 1.0 -inst rectlight0 "
+c = f"C:\\nonsys\\workspace\\GfxExp\\build\\bin\\Debug\\neural_radiance_caching.exe \
+    -cam-pos 0, 0, 0 \
+    -cam-roll -171.449 -cam-pitch -103.079 -cam-yaw -0.946 \
+    -brightness 1.0 \
+    -name scene -obj C:/nonsys/workspace/GfxExp/scene/ZeroDay_v1/MEASURE_SEVEN/MEASURE_SEVEN_COLORED_LIGHTS.fbx 1.0 simple_pbr \
+    -inst scene \
+    -name rectlight0 -emittance 1200 0 0 -rectangle 0.1 0.1 \
+    -begin-pos -0.2 1.05 2.586 -end-pos -0.2 1.05 3.0 -freq 5 -time 1.0 -inst rectlight0 "
 
 # c += f"-exp_name {exp_name} -frame_num 50 -save_img_every 5 "
 # c += f"-unbiased_restir "
